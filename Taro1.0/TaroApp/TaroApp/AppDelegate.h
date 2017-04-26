a0af41f08fc684188558756f6a23db805af28832
Apple <apple@AppledeMacBook-Pro.local>
2017-04-26 22:33:59 +0800

    no message

---
+++ Taro1.0/TaroApp/TaroApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TaroApp
+//
+//  Created by Apple on 17/4/26.
+//  Copyright © 2017年 taro.app. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Taro1.0/TaroApp/TaroApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TaroApp
+//
+//  Created by Apple on 17/4/26.
+//  Copyright © 2017年 taro.app. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
